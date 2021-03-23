a80bb573f6b9b5420c51a259c0a7d887122702d6
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-24 00:42:40 +0530

    Update test-validationtool.cpp

--- test-validationtool.cpp
+++ test-validationtool.cpp
@@ -4,34 +4,13 @@
 #include "sensor-validate.h"
 
 TEST_CASE("reports error when soc jumps abruptly") {
-   double tc1_Currentvalues[]={1,1,1,1.5,2};
-   double tc1_SOCvalues[]={1,2,3,4,5,15};
-  InputSignalConfig tc1_InputSignal[Max_InputSignal]={{tc1_Currentvalues,	sizeof(tc1_Currentvalues)/sizeof(double)},\
-															                        {tc1_SOCvalues,		sizeof(tc1_SOCvalues)/sizeof(double)}};
-
-  REQUIRE(validateReadings(tc1_InputSignal) == 0);
+  double socReadings[] = {0.0, 0.01, 0.5, 0.51};
+  int numOfSocReadings = sizeof(socReadings) / sizeof(socReadings[0]);
+  REQUIRE(validateSOCreadings(socReadings, numOfSocReadings) == 0);
 }
 
 TEST_CASE("reports error when current jumps abruptly") {
-        double tc2_Currentvalues[]={1,2,7,3,3};
-        double tc2_SOCvalues[]={1,2,3,4,5,6};
-
-        InputSignalConfig tc2_InputSignal[Max_InputSignal]={{tc2_Currentvalues,	sizeof(tc2_Currentvalues)/sizeof(double)},\
-															                              {tc2_SOCvalues,		sizeof(tc2_SOCvalues)/sizeof(double)}};
- 
-  REQUIRE(validateReadings(tc2_InputSignal) == 0);
-  
-}
-  
-TEST_CASE("reports error when NULL parameter passed") {
-        double tc2_Currentvalues[]={1,2,7,3,3};
-        double tc2_SOCvalues[]={1,2,3,4,5,6};
-
-        InputSignalConfig tc2_InputSignal[Max_InputSignal]={{NULL,	sizeof(tc2_Currentvalues)/sizeof(double)},\
-															                            {NULL,		sizeof(tc2_SOCvalues)/sizeof(double)}};
- 
-  REQUIRE(validateReadings(tc2_InputSignal) == 0);
-  
-  
-  
+  double currentReadings[] = {0.0, 0.02, 0.03, 0.33};
+  int numOfCurReadings = sizeof(currentReadings) / sizeof(currentReadings[0]);
+  REQUIRE(validateSOCreadings(currentReadings, numOfCurReadings) == 0);
 }
