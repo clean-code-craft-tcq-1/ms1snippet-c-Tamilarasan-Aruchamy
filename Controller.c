bbb20567b9b77ba7f2fb1a560f8aab3400dfec25
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-24 00:20:53 +0530

    adding controller c file

---
+++ Controller.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int mainController(InputSignalConfig InputSignal[Max_InputSignal])
+{
+int IsValidSignals=1;
+
+
+
+for(int Itr_Signal=0; Itr_Signal<Max_InputSignal; Itr_Signal++){
+		   if(InputSignal[Itr_Signal].NoiseDetected){
+			   IsValidSignals=0;
+		   }
+		   printf("\tNoiseDetected - %d\n",InputSignal[Itr_Signal].NoiseDetected);
+		}
+return IsValidSignals;
+
+}
