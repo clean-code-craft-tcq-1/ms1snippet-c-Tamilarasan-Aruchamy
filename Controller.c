a509517619c2ea1469dce8d282c7958e21e70e3f
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-24 22:39:15 +0530

    Printing Failure message

--- Controller.c
+++ Controller.c
@@ -9,10 +9,12 @@ int IsValidSignals=1;
 
 for(int Itr_Signal=0; Itr_Signal<Max_InputSignal; Itr_Signal++){
 		   if(InputSignal[Itr_Signal].NoiseDetected){
+		       printf("\t%s\n",FailureMessage[Itr_Signal].FailureMessage[DisplayLanguage]);
 			   IsValidSignals=0;
 		   }
-		   printf("\tNoiseDetected - %d\n",InputSignal[Itr_Signal].NoiseDetected);
+		   
 		}
 return IsValidSignals;
 
 }
+
