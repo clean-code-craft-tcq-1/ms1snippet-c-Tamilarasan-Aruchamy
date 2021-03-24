1865bd44576f1c689565ea91a57ed89dbe491993
Tamilarasan-Aruchamy <77611264+Tamilarasan-Aruchamy@users.noreply.github.com>
2021-03-24 22:37:39 +0530

    Updating Comments and adding Display Languages

--- sensor-validate.h
+++ sensor-validate.h
@@ -21,6 +21,28 @@ typedef  struct
 }InputSignalConfig;
 
 
-static const double NoiseLimit[Max_InputSignal]={2,5};
+enum Language
+{
+	English,
+	German,
+	Language_Max  // Add the new languages above this variable only.
+};
+
+typedef struct
+{
+	char FailureMessage [Language_Max][200];
+}FailureMessageConfig;
+
+
+int DisplayLanguage = English; // Default Language is English
+
+
+static const double NoiseLimit[Max_InputSignal]={2,5}; // Index 0 - MaxDelta_CurrentSignal,1 - MaxDelta_SOCSignal
+
+static const FailureMessageConfig FailureMessage[Max_InputSignal]= { \
+		/* Index 0-In English , 1- In German */ \
+	 {"Noise Detected in Current Input Signal","Rauschen im Stromeingangssignal erkannt"}, 	/* FailureMessage when Noise in Current Input */ \
+	 {"Noise Detected in SOC Input Signal","Rauschen im SOC-Eingangssignal erkannt"}};	/* FailureMessage when Noise in SOC Input */
+
 
 
